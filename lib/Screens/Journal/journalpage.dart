93f2536dfe2140ccc3959e405687173abd33c5d1
Siddharth Pugalia <sidpug@gmail.com>
2022-07-15 21:06:40 +0530

    errors fixed for the time

--- lib/Screens/Journal/journalpage.dart
+++ lib/Screens/Journal/journalpage.dart
@@ -1,17 +1,15 @@
 import 'dart:developer';
 
-import 'package:amigoapp/Models/journalModel.dart';
-import 'package:amigoapp/Screens/Journal/addjournal.dart';
-import 'package:amigoapp/Services/auth.dart';
-import 'package:amigoapp/Utils/color_constants.dart';
-import 'package:amigoapp/Utils/constant_components.dart';
-import 'package:amigoapp/Utils/size_config.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import '../../Models/journalModel.dart';
+import '../../Screens/Journal/addjournal.dart';
+import '../../Services/auth.dart';
+import '../../Utils/color_constants.dart';
+import '../../Utils/constant_components.dart';
+import '../../Utils/size_config.dart';
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
 
 class JournalPage extends StatefulWidget {
-  const JournalPage({Key key}) : super(key: key);
+  const JournalPage({super.key});
 
   @override
   _JournalPageState createState() => _JournalPageState();
@@ -33,204 +31,111 @@ class _JournalPageState extends State<JournalPage> {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance
-          .collection('users')
-          .doc(AuthServices().returnUserUID())
-          .collection('journal')
-          .where('date', isEqualTo: Timestamp.fromDate(selectedDate).toDate())
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          listOfJournals = [];
-          try {
-            for (var doc in snapshot.data.docs) {
-              JournalModel currentReview = JournalModel.fromJson(doc.data());
-              listOfJournals.add(currentReview);
-            }
-          } catch (e) {
-            log(e.toString());
-          }
-        }
-        return Scaffold(
-          drawer: appDrawer(context),
-          appBar: AppBar(
-            backgroundColor: amigo_white,
-            centerTitle: true,
-            leading: Builder(
-              builder: (context) => IconButton(
-                  icon: Icon(
-                    Icons.menu,
-                    size: SizeConfig.heightMultiplier * 2.5,
-                    color: amigo_grey,
-                  ),
-                  onPressed: () {
-                    Scaffold.of(context).openDrawer();
-                  }),
-            ),
-            title: Text(
-              'Journal',
-              style: regulartextstyle(
-                SizeConfig.appBarTextSize,
-                amigo_grey,
-                false,
+    return Scaffold(
+      drawer: appDrawer(context),
+      appBar: AppBar(
+        backgroundColor: amigo_white,
+        centerTitle: true,
+        leading: Builder(
+          builder: (context) => IconButton(
+              icon: Icon(
+                Icons.menu,
+                size: SizeConfig.heightMultiplier! * 2.5,
+                color: amigo_grey,
               ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              }),
+        ),
+        title: Text(
+          'Journal',
+          style: regulartextstyle(
+            20,
+            amigo_grey,
+            false,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.add,
+              color: amigo_grey,
             ),
-            actions: [
-              IconButton(
-                icon: Icon(
-                  Icons.add,
-                  color: amigo_grey,
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => JournalAdd(),
                 ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => JournalAdd(
-                        selectedDate: selectedDate,
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ],
+              );
+            },
           ),
-          body: SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: SizeConfig.widthMultiplier * 4,
-              ),
-              child: Column(
-                children: [
-                  Container(
-                      child: TableCalendar(
-                    headerStyle: HeaderStyle(
-                      titleCentered: true,
-                      formatButtonVisible: false,
-                      titleTextStyle: regulartextstyle(
-                        SizeConfig.appBarTextSize,
-                        amigo_blue,
-                        true,
-                      ),
-                    ),
-                    calendarStyle: CalendarStyle(
-                      selectedDecoration: BoxDecoration(
-                        color: amigo_blue,
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                    firstDay: DateTime(2015),
-                    lastDay: presentDate,
-                    focusedDay: focusedDate,
-                    selectedDayPredicate: (day) {
-                      return isSameDay(selectedDate, day);
-                    },
-                    onDaySelected: (selectedDay, focusedDay) async {
-                      setState(() {
-                        selectedDate = selectedDay;
-                        focusedDate = focusedDay;
-                      });
-                    },
-                    onPageChanged: (focusedDay) {
-                      focusedDate = focusedDay;
-                    },
-                  )),
-                  SizedBox(
-                    height: SizeConfig.heightMultiplier * 2,
-                  ),
-                  listOfJournals.length == 0
-                      ? Padding(
-                          padding: EdgeInsets.only(
-                            top: SizeConfig.heightMultiplier * 4,
-                          ),
-                          child: Text(
-                            "Tap on + to add an entry",
-                            textAlign: TextAlign.center,
-                            style: regulartextstyle(
-                              SizeConfig.regulartextSize,
-                              amigo_lightGrey,
-                              false,
-                            ),
-                          ),
-                        )
-                      : Column(
-                          children: List.generate(
-                            listOfJournals.length,
-                            (index) => journal(
-                              listOfJournals[index],
-                              index,
-                            ),
-                          ),
-                        ),
-                ],
-              ),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  Widget journal(JournalModel givenjournal, int index) {
-    Color journalcolor;
-    if (givenjournal.color == null)
-      journalcolor = amigo_lightBlue;
-    else
-      journalcolor = Color(givenjournal.color);
-
-    return GestureDetector(
-      onTap: () {
-        tapped = !tapped;
-        setState(() {
-          if (tapped)
-            tappedIndex = index;
-          else
-            tappedIndex = -1;
-        });
-      },
-      child: Card(
-        margin: EdgeInsets.symmetric(vertical: SizeConfig.heightMultiplier),
-        elevation: 10,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        ],
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
         child: Container(
-          width: double.infinity,
           padding: EdgeInsets.symmetric(
-            horizontal: SizeConfig.widthMultiplier * 3,
-            vertical: SizeConfig.heightMultiplier * 1.5,
-          ),
-          decoration: BoxDecoration(
-            color: journalcolor,
-            borderRadius: BorderRadius.circular(20),
+            horizontal: SizeConfig.widthMultiplier! * 4,
           ),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                givenjournal.journalTitle,
-                style: regulartextstyle(
-                  SizeConfig.regulartextSize,
-                  amigo_grey,
-                  true,
-                ),
-              ),
-              SizedBox(
-                height: 5.0,
-              ),
-              Text(
-                givenjournal.journalBody,
-                maxLines: tappedIndex == index ? null : 1,
-                overflow: tappedIndex == index ? null : TextOverflow.ellipsis,
-                style: regulartextstyle(
-                  SizeConfig.regulartextSize,
-                  amigo_grey,
-                  false,
-                ),
-              )
-            ],
+            children: const [],
           ),
         ),
       ),
     );
   }
+}
+
+Widget journal(JournalModel givenjournal, int index) {
+  Color journalcolor;
+  if (givenjournal.color == null) {
+    journalcolor = amigo_lightBlue;
+  } else {
+    journalcolor = Color(givenjournal.color);
+  }
+
+  return GestureDetector(
+    onTap: () {},
+    child: Card(
+      margin: EdgeInsets.symmetric(vertical: SizeConfig.heightMultiplier!),
+      elevation: 10,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(
+          horizontal: SizeConfig.widthMultiplier! * 3,
+          vertical: SizeConfig.heightMultiplier! * 1.5,
+        ),
+        decoration: BoxDecoration(
+          color: journalcolor,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              givenjournal.journalTitle,
+              style: regulartextstyle(
+                20,
+                amigo_grey,
+                true,
+              ),
+            ),
+            const SizedBox(
+              height: 5.0,
+            ),
+            Text(
+              givenjournal.journalBody,
+              style: regulartextstyle(
+                20,
+                amigo_grey,
+                false,
+              ),
+            )
+          ],
+        ),
+      ),
+    ),
+  );
 }
